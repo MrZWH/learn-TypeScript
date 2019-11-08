f2d0625489201efe88e0ec7afbcc7822389b10ca
MrZWH <blesszhang@foxmail.com>
2019-11-08 17:56:53 +0800

    取消功能添加

--- ts-axios/src/core/xhr.ts
+++ ts-axios/src/core/xhr.ts
@@ -1,82 +1,80 @@
-import {
-	AxiosRequestConfig,
-	AxiosPromise,
-	AxiosResponse
-} from '../types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
 
-import {parseHeaders} from '../helpers/headers'
+import { parseHeaders } from '../helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-	return new Promise((resolve, reject) => {
-		const {
-			data = null,
-			url,
-			method = 'get',
-			headers,
-			responseType,
-			timeout,
-		} = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout, cancelToken } = config
 
-		const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-		if(responseType) {
-			request.responseType = responseType
-		}
+    if (responseType) {
+      request.responseType = responseType
+    }
 
-		if (timeout) {
-			request.timeout = timeout
-		}
+    if (timeout) {
+      request.timeout = timeout
+    }
 
-		request.onreadystatechange = function handleLoad () {
-			if(request.readyState !== 4) {
-				return
-			}
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
 
-			// 网络错误或超时错误时
-			if (request.status === 0) {
-				return
-			}
+      // 网络错误或超时错误时
+      if (request.status === 0) {
+        return
+      }
 
-			const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-			const responseData = responseType !== 'text' ? request.response : request.responseText
-			const response: AxiosResponse = {
-				data: responseData,
-				status: request.status,
-				statusText: request.statusText,
-				headers: responseHeaders,
-				config,
-				request
-			}
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
 
-			handleResponse(response)
-		}
+      handleResponse(response)
+    }
 
-		request.onerror = function handleError() {
-			reject(new Error('Network error'))
-		}
+    request.onerror = function handleError() {
+      reject(new Error('Network error'))
+    }
 
-		request.ontimeout = function handleTimeout() {
-			reject(new Error(`Timeout of ${timeout} ms exceeded`))
-		}
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+    }
 
-		request.open(method.toUpperCase(), url!, async: true)
+    request.open(method.toUpperCase(), url!, true)
 
-		Object.keys(headers).forEach(name => {
-			if (data === null && name.toLowerCase() === 'content-type') {
-				delete headers[name]
-			} else {
-				request.setRequestHeaders(name, headers[name])
-			}
-		})
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
 
-		request.send(data)
+    if (cancelToken) {
+      cancelToken.promise
+        .then(reason => {
+          request.abort()
+          reject(reason)
+        })
+        .catch(e => console.log(e))
+    }
 
-		function handleResponse(response: AxiosResponse): void {
-			if (response.status >= 200 && responseType.status < 300) {
-				resolve(response)
-			} else {
-				reject(new Error(`Request failed with status code ${response.status}`))
-			}
-		}
-	})
+    request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`))
+      }
+    }
+  })
 }
